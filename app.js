b7f7121cba0510f538e2dad90ad70c74e2959299
EmilioCarballo-rgb <carballoemilio75@gmail.com>
2026-04-06 23:41:49 -0300

    E-comerce

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+
+const app = express();
+const port = 3000;
+
+app.set ("view engine", "ejs");
+
+const user = {
+    firstName: 'E-Comerce',
+    secondName: 'De La Banda',
+    isAdmin: true,
+    
+};
+
+const products = [
+	{ title: "Mate", description: "De porongo." },
+	{ title: "Yerba baldo", description: "Segun el gordo esta barata." },
+	{ title: "Bombilla", description: "De cuero encurtido." },
+]
+
+app.get("/", (req, res )=> {res.render("pages/index", {user, products})});
+
+
+
+app.listen(port, ()=> console.log("Server abierto como el culo del flaco"));
