5ded7c3c6b47d5c17911a69538788b368b7aadf2
FrankSibona <francosibona01@gmail.com>
2026-04-09 17:58:48 -0300

    Check out terminado

--- app.js
+++ app.js
@@ -193,9 +193,30 @@ app.get("/cart", (req, res)=> {
     res.render("pages/cart", { products: cart, user, total: totalAcumulado });
 });
 
-app.get("/checkout", (req, res)=> {res.render ("pages/checkout", {products, user})})
+app.get("/checkout", (req, res) => {
+    let totalCalculado = 0;
+
+    for (let producto of cart) {
+        // 1. Nos aseguramos de que el precio exista. Si no existe, usamos "0"
+        let precioActual = producto.price || "0"; 
+
+        // 2. Convertimos el valor a String a la fuerza, y recién ahí le aplicamos el replace
+        let precioLimpio = parseInt(String(precioActual).replace(/[^0-9]/g, ''));
+        
+        // 3. Sumamos al total (por las dudas validamos que sea un número válido)
+        if (!isNaN(precioLimpio)) {
+            totalCalculado += (precioLimpio * producto.quantity);
+        }
+    }
+
+    res.render("pages/checkout", { 
+        user: user, 
+        cart: cart, 
+        total: totalCalculado 
+    });
+});
 app.get("/register", (req, res)=> {res.render ("pages/register", {products, user})})
-app.get("/login", (req, res)=> {res.render ("pages/login", {products,user })})
+app.get("/login", (req, res)=> {res.render ("pages/login", {products})})
 
 app.post("/register", (req, res) => { res.redirect("/"); });
 app.post("/login", (req, res) => { res.redirect("/"); });
