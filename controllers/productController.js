d600ff26560545b6a863480776abdb29c74213a2
EmilioCaheMenna <emiliocahemenna@gmail.com>
2026-05-11 15:01:05 -0300

    Esta lista la story9

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ const productController = {
     
     // VISTA HOME: Los más pedidos 
     getIndex: (req, res) => {
-        let allProducts = Product; 
+        let allProducts = Product.findAll(); 
         let mostWanted = allProducts.filter(p => p.isMostWanted === true);
         
         if (mostWanted.length < 10) {
@@ -25,7 +25,7 @@ const productController = {
     // VISTA POR CATEGORÍA
     getCategory: (req, res) => {
         const categoriaElegida = req.params.categoriaId;
-        const productosFiltrados = Product.filter(p => p.category === categoriaElegida);
+        const productosFiltrados = Product.findByCategory(categoriaElegida);
         res.render("pages/index", { 
             products: productosFiltrados, 
             mostWanted: [] 
@@ -34,25 +34,26 @@ const productController = {
 
     // VISTA DETALLE + RELACIONADOS 
     getProductById: (req, res) => {
-        const idSeleccionado = parseInt(req.params.id); 
-        const productoEncontrado = Product.find(p => p.id === idSeleccionado);
+        const idSeleccionado = req.params.id; 
+        const productoEncontrado = Product.findById(idSeleccionado);
 
         if (productoEncontrado) {
-            // Lógica de Productos Relacionados
-            let relacionados = Product.filter(p => 
+            // Lógica de Productos Relacionados (mismo rubro, distinto ID)
+            let relacionados = Product.findAll().filter(p => 
                 p.category === productoEncontrado.category && p.id !== productoEncontrado.id
             );
 
-            // Si hay más de 4, elegimos 4 al azar
+            // Seleccionamos máximo 4 al azar si hay muchos
             if (relacionados.length > 4) {
                 relacionados = relacionados.sort(() => 0.5 - Math.random()).slice(0, 4);
             }
 
             res.render("pages/products", { 
-                products: productoEncontrado, 
+                product: productoEncontrado, 
                 relacionados: relacionados 
             });
         } else {
+            // Escenario 2: El producto no existe -> 404
             res.status(404).render("pages/404");
         }
     }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,9 @@
+// routes/productRoutes.js
 const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-
 router.get('/', productController.getIndex); 
-
 router.get('/category/:categoriaId', productController.getCategory);
 router.get('/products/:id', productController.getProductById);
 
