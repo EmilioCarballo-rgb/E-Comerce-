a059a572dc1f30453365559ffc33bd72c6c9c9ef
Tomás Baigorria23 <tucorreoejemplo.com>
2026-07-24 14:17:34 -0300

    Relizado el story 1

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,6 +6,8 @@ router.get('/', productController.getIndex);
 // Actualizamos la ruta para que coincida con la US #10
 router.get('/categories/:category', productController.getCategory);
 router.get('/search', productController.search);
-router.get('/products/:id', productController.getProductById);
+
+// Le quitamos "/products" y dejamos solo "/:id"
+router.get('/:id', productController.getProductById);
 
 module.exports = router;
